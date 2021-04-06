ec6ca2907514572fb1a2690a7a4dce7dc8aa4874
Louis <louis923236960@gmail.com>
2021-04-06 23:51:02 +0800

    difficulty mod3

--- random/difficulty.cpp
+++ random/difficulty.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 #include "../class/game/gameStruct.h"
 
-namespace data{
-    struct Resource
-    {
-        double food = 0;
-        double equipment = 0;
-        int manpower = 0;
-        int manpowerInUse;
-        double baseLand = 0;
-        double usedLand = 0;
-        double capturedLand = 0;
-        int camp = 100;
-        int campUsed = 0;
-        int airport = 0;
-        int airportUsed = 0;
+data::Resource resource;
 
-        double baseLandMul = 1;
-        double baseLandTroopMul = 1;
-        double baseAirTroopMul = 1;
-        double baseTankMul = 1;
-        double baseAirToopMul2 = 1;
-        double baseTrainingTimeMul = 1;
-
-        double baseRecoveryDiff = 0;
-    };
-    
+void difficulty(data::Resource& resource, int diff){
+    double foodx = 0;
+    double foody = 20;
+    if (diff == 1){
+        foodx = foody * 0.7;
+    }
+    if (diff == 2){
+        foodx = foody * 0.65;
+        foody = foody * 0.8;
+    }
+    if (diff == 3){
+        foodx = foody * 0.55;
+        foody = foody * 0.7;
+    }
+    if (diff == 4){
+        foodx = foody * 0.4;
+        foody = foody * 0.55;
+    }
+    if (diff == 5){
+        foodx = foody * 0.3;
+        foody = foody * 0.2;
+    }
+    int food;
+    int range = foody - foodx +1;
+    srand(time(NULL));
+    food = rand() % range + foodx;
+    resource.food = food;
+    std::cout << food;
 }
 
-int main(int x, int y, data::Resource&){
-
+int main(){
+    difficulty(resource,3);
 }
-
